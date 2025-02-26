b60502530db38bbfe4781da5be83ca581b1b30b6
jerzy szajner <jerzy.szajner@gmail.com>
2025-02-26 10:06:51 +0100

    implemented open and close modal feature

---
+++ src/js/app.js
@@ -0,0 +1,24 @@
+import { openModal, closeModal } from "./modal";
+
+// Selecting DOM elements
+const formModal = document.querySelector(".form-modal");
+const form = document.querySelector(".form");
+const openModalButton = document.querySelector(".tools__button--add");
+const closeModalButton = document.querySelector(".form__close-button");
+const titleInput = document.querySelector(".form__title-input");
+const dateInput = document.querySelector(".form__date-input");
+const timeInput = document.querySelector(".form__time-input");
+const categorySelect = document.querySelector(".form__category-select");
+const prioritySelect = document.querySelector(".form__priority-select");
+const openChartButton = document.querySelector(".tools__button--chart");
+const filterSelect = document.querySelector(".tools__filter-month");
+const submitButton = document.querySelector(".form__submit-button");
+const formSubmissionFeedback = document.querySelector(
+  ".form__submission-feedback"
+);
+
+// Adding event listeners to the DOMContentLoaded event
+document.addEventListener("DOMContentLoaded", () => {
+  openModal(formModal, openModalButton);
+  closeModal(formModal, closeModalButton);
+});

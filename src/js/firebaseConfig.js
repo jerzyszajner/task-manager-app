00ca0d80eddb7340cd65ae3fc37768cb9ae4ec21
jerzy szajner <jerzy.szajner@gmail.com>
2025-02-26 10:32:21 +0100

    establish firebase connection

---
+++ src/js/firebaseConfig.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDHpRUkZWV5zxeRv4DCYwr3PM0BMi-aR6I",
+  authDomain: "task-manager-dfa16.firebaseapp.com",
+  projectId: "task-manager-dfa16",
+  storageBucket: "task-manager-dfa16.firebasestorage.app",
+  messagingSenderId: "129425047764",
+  appId: "1:129425047764:web:e6f2bea4d196b8d3e23b41",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
